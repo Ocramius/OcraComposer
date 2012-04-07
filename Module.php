f48418691221ae3c013d27a43c33789ce358db34
Marco Pivetta <ocramius@gmail.com>
2012-04-08 00:28:53 +0200

    Adding logic to download the composer installer and aid the user
    This simply throws a RuntimeException when the composer autoloader cannot be found, but first tries to download and install it

--- Module.php
+++ Module.php
@@ -2,6 +2,72 @@
 
 namespace OcraComposer;
 
+use UnexpectedValueException;
+use RuntimeException;
+use Zend\Module\Manager;
+
 class Module
 {
+    public function init(Manager $moduleManager)
+    {
+        $moduleManager->events()->attach('loadModules.post', array($this, 'modulesLoaded'));
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function modulesLoaded($e)
+    {
+        $config = $e->getConfigListener()->getMergedConfig();
+
+        $autoloadFile = $config->composer->installation_directory . '/' . $config->composer->autoload_file;
+
+        if (!@include $autoloadFile) {
+
+            $installer = realpath($config->composer->installer_filename);
+            $installer = $installer ? $installer : $this->downloadComposerInstaller($config);
+
+            throw new RuntimeException(
+                'Could not locate "'
+                . $config->composer->installation_directory . '/' . $config->composer->autoload_file . "\", "
+                . "you probably didn't install all required project dependencies.\n"
+                . 'Composer has been downloaded to "' . $installer . "\".\n"
+                . 'Install it by running `php ' . $config->composer->installer_filename . "`, "
+                . 'then install dependencies by running `php composer.phar install`.'
+            );
+        }
+    }
+
+    protected function downloadComposerInstaller($config)
+    {
+        if(!is_writable(getcwd())) {
+            throw new RuntimeException(
+                '"' . realpath(getcwd()) . '" is not writeable, could not download composer installer to it'
+            );
+        }
+
+        if (!$installer = @file_get_contents($config->composer->installer_download_location)) {
+            throw new UnexpectedValueException(
+                'Could not locate or download composer installer from "'
+                . $config->composer->installer_download_location . '"'
+            );
+        }
+
+        if(!@file_put_contents($config->composer->installer_filename, $installer)) {
+            throw new UnexpectedValueException(
+                'Could not write composer installer to "' . $config->composer->installer_filename . '"'
+            );
+        }
+
+        if(!$installer = realpath($config->composer->installer_filename)) {
+            throw new UnexpectedValueException(
+                'Could not find downloaded composer installer "' . $config->composer->installer_filename . '"'
+            );
+        }
+
+        return $installer;
+    }
+
 }
--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # OcraComposer
 
-This module aims to provide (for now) simple autoloading 
-for [Composer](https://github.com/composer/composer) based
-[Zend Skeleton Application](https://github.com/zendframework/ZendSkeletonApplication)
-installations.
+This module aims to provide (for now) simple autoloading for [Composer](https://github.com/composer/composer) based
+[Zend Skeleton Application](https://github.com/zendframework/ZendSkeletonApplication) installations.
 
 # Usage
 
-  1. Setup this module as a dependency in your module's or application's `composer.json`
-     (the [packagist](http://packagist.org) package name is 
-     [ocramius/OcraComposer](http://packagist.org/packages/ocramius/OcraComposer).
+  1. Setup a `composer.json` file in your application, add this module
+     ([ocramius/OcraComposer](http://packagist.org/packages/ocramius/OcraComposer)) and any number of other modules
+     as it's dependencies.
   2. Setup your `config/application.config.php` to load the `OcraComposer` module
   3. Add `vendor/ocramius` to your module paths in `config/application.config.php`
 
