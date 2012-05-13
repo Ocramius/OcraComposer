6e86e958e1707a1f4fa8ce5abe7852e343fcf2a4
Marco Pivetta <ocramius@gmail.com>
2012-05-13 20:28:38 +0200

    Enforcing autoloading during init (avoiding useless delays which would cause problems in module autoloading)

--- Module.php
+++ Module.php
@@ -8,69 +8,78 @@ use Zend\Module\Manager;
 
 class Module
 {
+    /**
+     * @var null|array
+     */
+    protected $config = null;
+
+    /**
+     * @param \Zend\Module\Manager $moduleManager
+     * @throws \RuntimeException
+     */
     public function init(Manager $moduleManager)
     {
-        $moduleManager->events()->attach('loadModules.post', array($this, 'modulesLoaded'));
-    }
+        $config = $this->getConfig();
 
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function modulesLoaded($e)
-    {
-        $config = $e->getConfigListener()->getMergedConfig();
-        $autoloadFile = $config->composer->installation_directory . '/' . $config->composer->autoload_file;
-
-        if (!@include $autoloadFile) {
-
-            $installer = realpath($config->composer->installer_filename);
+        if(!@include_once $config['ocra_composer']['autoload_file_path']) {
+            $installer = realpath($config['ocra_composer']['installer_filename']);
             $installer = $installer ? $installer : $this->downloadComposerInstaller($config);
 
             throw new RuntimeException(
                 'Could not locate "'
-                . $config->composer->installation_directory . '/' . $config->composer->autoload_file . "\", "
-                . "you probably didn't install all required project dependencies.\n"
-                . 'Composer has been downloaded to "' . $installer . "\".\n"
-                . 'Install it by running `php ' . $config->composer->installer_filename . "`, "
-                . 'then install dependencies by running `php composer.phar install`.'
+                    . $config['ocra_composer']['autoload_file_path'] . "\", "
+                    . "you probably didn't install all required project dependencies.\n"
+                    . 'Composer has been downloaded to "' . $installer . "\".\n"
+                    . 'Install it by running `php ' . $config['ocra_composer']['installer_filename'] . "`, "
+                    . 'then install dependencies by running `php composer.phar install`.'
             );
         }
     }
 
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        if (!$this->config) {
+            $this->config = include __DIR__ . '/config/module.config.php';
+        }
+
+        return $this->config;
+    }
+
     protected function downloadComposerInstaller($config)
     {
-        if (!$config->composer->automatically_download_installer) {
+        if (!$config['ocra_composer']['automatically_download_installer']) {
             throw new RuntimeException(
                 'Could not locate "'
-                . $config->composer->installation_directory . '/' . $config->composer->autoload_file . "\", "
+                . $config['ocra_composer']['autoload_file_path'] . "\", "
                 . "you probably didn't install all required project dependencies through composer.\n"
             );
         }
 
         if (!is_writable(getcwd())) {
             throw new RuntimeException(
-                '"' . realpath(getcwd()) . '" is not writeable, could not download composer installer to it'
+                '"' . realpath(getcwd()) . '" is not writable, could not download composer installer to it'
             );
         }
 
-        if (!$installer = @file_get_contents($config->composer->installer_download_location)) {
+        if (!$installer = @file_get_contents($config['ocra_composer']['installer_download_location'])) {
             throw new UnexpectedValueException(
                 'Could not locate or download composer installer from "'
-                . $config->composer->installer_download_location . '"'
+                . $config['ocra_composer']['installer_download_location'] . '"'
             );
         }
 
-        if (!@file_put_contents($config->composer->installer_filename, $installer)) {
+        if (!@file_put_contents($config['ocra_composer']['installer_filename'], $installer)) {
             throw new UnexpectedValueException(
-                'Could not write composer installer to "' . $config->composer->installer_filename . '"'
+                'Could not write composer installer to "' . $config['ocra_composer']['installer_filename'] . '"'
             );
         }
 
-        if (!$installer = realpath($config->composer->installer_filename)) {
+        if (!$installer = realpath($config['ocra_composer']['installer_filename'])) {
             throw new UnexpectedValueException(
-                'Could not find downloaded composer installer "' . $config->composer->installer_filename . '"'
+                'Could not find downloaded composer installer "' . $config['ocra_composer']['installer_filename'] . '"'
             );
         }
 
